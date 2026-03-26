d543f806a7df4c9f8704cd6b7eedc3c8e42402ae
Max <maxbuchd@outlook.com>
2026-03-26 14:48:30 +0800

    chore/cleanup: remove dead re-exports from src/utils/index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,27 +1,4 @@
 export {
-  generateRemotionProject,
-  type GenerateProjectInput,
-  type GenerateProjectOutput,
-} from "./remotion-project-generator.js";
-export {
-  renderVideo,
-  type RenderVideoInput,
-  type RenderVideoOutput,
-} from "./video-renderer.js";
-export {
-  generateSrt,
-  type GenerateSrtInput,
-  type GenerateSrtOutput,
-} from "./srt-generator.js";
-export {
-  cleanupTempFiles,
-  cleanupRemotionTempDir,
-  DEFAULT_PRESERVE_PATTERNS,
-  type CleanupOptions,
-  type CleanupResult,
-  type CleanupTempOptions,
-} from "./cleanup.js";
-export {
   VideoGenerationErrorCode,
   VideoGenerationError,
   ValidationError,
